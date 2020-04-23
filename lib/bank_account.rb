ed20e79373b4a78c62dc5c35a01b1bcdbcb17a91
John Carter <johncarter352@gmail.com>
2020-04-23 06:32:23 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,9 +1,11 @@
-class BankAccount
+class 
    attr_reader :name 
   
-  def initialize(name, balance = "1000")
+  def initialize(name)
     @name = name 
     @balance = balance
+    @status = "open"
+  end
     
 
 end
