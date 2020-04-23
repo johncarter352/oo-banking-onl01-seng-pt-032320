03910c0ec6a60a1f385a41468bd46788ba1a60b1
John Carter <johncarter352@gmail.com>
2020-04-23 07:12:01 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -16,5 +16,16 @@ class Transfer
    false 
     end
  end
+ 
+ def execute_transaction
+    if valid? && @status == "pending" && @sender.balance  >= @amount
+      @sender.balance -= @amount 
+      @receiver.balance += @amount
+      @status = "complete"
+    else 
+      @status = "rejected"
+      "Transaction rejected. Please check your account balance."
+    end 
+  end
   
 end
